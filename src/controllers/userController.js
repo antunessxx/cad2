cab1101fe6b3959344581234fb3bfd0c7ec1ccad
Kauany Antunes <kauanyantunes165@gmail.com>
2024-11-18 08:36:00 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 
-// Gerar token
 const generateToken = (user) => {
     return jwt.sign(
         { username: user.username, email: user.email },
@@ -11,13 +10,11 @@ const generateToken = (user) => {
     );
 };
 
-// Cadastro de usuário
 const signup = async (req, res) => {
     console.log("Dados recebidos no cadastro:", req.body);
     
     const { username, email, password } = req.body;
 
-    // Verificar se o usuário já existe
     const existingUser = userModel.findUserByUsername(username) || userModel.findUserByEmail(email);
     if (existingUser) {
         console.log("Usuário já existe:", existingUser);
@@ -25,14 +22,14 @@ const signup = async (req, res) => {
     }
 
     try {
-        // Criptografar a senha
+    
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Criar o novo usuário
+        
         const user = { username, email, password: hashedPassword };
         userModel.addUser(user);
 
-        // Gerar token e enviar resposta
+      
         const token = generateToken(user);
         res.status(201).json({ message: 'Usuário criado com sucesso!', token });
     } catch (err) {
@@ -42,28 +39,28 @@ const signup = async (req, res) => {
 };
 
 
-// Login de usuário
+
 const login = async (req, res) => {
     const { username, password } = req.body;
 
-    // Verificar se o usuário existe
+    
     const user = userModel.findUserByUsername(username);
     if (!user) {
         return res.status(400).json({ message: 'Usuário ou senha inválidos.' });
     }
 
-    // Verificar senha
+   
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         return res.status(400).json({ message: 'Usuário ou senha inválidos.' });
     }
 
-    // Retornar token
+  
     const token = generateToken(user);
     res.status(200).json({ token });
 };
 
-// Listar todos os usuários
+
 const getAllUsers = (req, res) => {
     res.json(userModel.getAllUsers());
 };
