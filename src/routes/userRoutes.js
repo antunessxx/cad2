02778301c0b4e00d3d54b910e5b6c0a57f518447
Kauany Antunes <kauanyantunes165@gmail.com>
2024-11-18 08:34:36 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,11 +4,11 @@ const authenticateToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-// Rotas de autenticação
+
 router.post('/signup', signup);
 router.post('/login', login);
 
-// Rota protegida para listar usuários
+
 router.get('/users', authenticateToken, getAllUsers);
 
 module.exports = router;
